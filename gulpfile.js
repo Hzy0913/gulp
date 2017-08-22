e313780bb90c0f73ee932a25f250fdc3da19f8ad
韩兆赟 <758858816@qq.com>
2017-08-22 20:30:53 +0800

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -13,18 +13,12 @@ var gulp = require('gulp'),
     useref = require('gulp-useref'),
     gulpSequence = require('gulp-sequence')
 
-// 编译less，其中plumber是防止出错崩溃的插件
-gulp.task('less', function() {
-    gulp.src('less/*.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(gulp.dest('css'));
-});
+
 
 
 // 压缩html
 //************************
-//替换html内容
+//如果你想要换html内容，如下
 //.pipe(replace('<link rel="stylesheet" href="css/style.css">',' <link rel="stylesheet" href="css/main.min.css">'))
 //.pipe(replace('<script src="js/jquery.min.js"></script>','<script src="js/main.min.js"></script>'))
 //************************
@@ -36,7 +30,7 @@ gulp.task('htmlmin', function () {
         removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
         removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
         removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
-        //minifyJS: true,//压缩页面JS
+        minifyJS: true,//压缩页面JS
         minifyCSS: true//压缩页面CSS
     };
     gulp.src('dist/*.html')
@@ -44,20 +38,21 @@ gulp.task('htmlmin', function () {
         .pipe(gulp.dest('dist/'));
 });
 
-//压缩图片
+//压缩图片(压缩图片效果比较一般)
 gulp.task('imgmin', function () {
     gulp.src('img/*.{png,jpg,gif,ico}')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
 });
-//key  3>2
+//压缩图片(使用tinypng压缩图片效果很好,需要去tinypng官网申请密钥,而且每月每个密钥限500张)
+//key3>2
 //If2g1qF1A4jQRa9MtTYt9ypAvCyS4JL8
 //ctq8a2dQWrZHA9BDcrfSZafIko9OyJAC
 gulp.task("minpng", function(){
     gulp.src('img/**/*.{png,jpg,jpeg}')
         .pipe(tinypng({
             key: 'If2g1qF1A4jQRa9MtTYt9ypAvCyS4JL8',
-            sigFile: 'images/.tinypng-sigs',
+            sigFile: '',
             log: true
         }))
         .pipe(gulp.dest('dist/img'));
@@ -65,14 +60,7 @@ gulp.task("minpng", function(){
 
 
 
-// minall  压缩js css
-gulp.task('allsrc', function () {
-    return gulp.src('*.html')
-        .pipe(useref())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
-});
+
 // useref  链接 css js
 gulp.task('mincon', function () {
     return gulp.src('*.html')
@@ -86,7 +74,7 @@ gulp.task('mincss', function() {
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist/css'));
 });
-
+// 压缩js，存到dist/js
 gulp.task('minjs', function() {
     gulp.src(['dist/js/*.js'])
         .pipe(uglify())
@@ -94,32 +82,27 @@ gulp.task('minjs', function() {
 });
 
 
-// 将所有css文件连接为一个文件并压缩，存到dest/css
-gulp.task('concatCss', function() {
-    gulp.src(['css/*.css'])
-        .pipe(concat('main.min.css'))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/css'));
-});
-// 将所有js文件连接为一个文件并压缩，存到dest/js
-// 按顺序合并js  gulp.src(['js/public.js','js/main.js','js/mobile.main.js'])
-gulp.task('concatJs', function() {
-    gulp.src(['js/jquery.min.js','js/skrollr.js','js/main.js'])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
+
+//gulp 命令
 
 
-//  压缩项目(css)
-gulp.task( 'minpro', gulpSequence( 'mincon', 'mincss','imgmin','htmlmin' ) );
-//  压缩项目(css,js)
+//  压缩项目(css,js,html,img(一般效果压缩))
 gulp.task( 'minall', gulpSequence( 'mincon', 'mincss', 'minjs','imgmin','htmlmin' ) );
-
-
-gulp.task('min', ['htmlmin','concatCss','concatJs','imgmin']);
-// 默认任务
+// 默认任务  自动刷新页面，编译less
 gulp.task('default',['watch']);
+
+
+
+
+
+
+// 编译less，其中plumber是防止出错崩溃的插件
+gulp.task('less', function() {
+    gulp.src('css/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest('css'));
+});
 
 
 // 监听任务
@@ -135,15 +118,13 @@ gulp.task('watch', function() {
     //// 预处理
     //gulp.watch('src/jade/**', ['jade']);
     server: "./"
-    //gulp.watch('src/coffee/**', ['coffee']);
 
 
     //// 预处理less
-    gulp.watch('less/**', ['less']);
-    
+    gulp.watch('css/**.less', ['less']);
 
 
-    // 自动刷新
+    // 自动刷新css和js
     gulp.watch('css/**', function() {
         browserSync.reload();
     });
